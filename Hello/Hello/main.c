5b3e9c9995f30cecb933c031a83147e3eae0292d
softdeveloper2 <qianfeng@Macintosh-3.local>
2015-08-12 17:49:22 +0800

    haha

---
+++ Hello/Hello/main.c
@@ -0,0 +1,56 @@
+//
+//  main.c
+//  Hello
+//
+//  Created by zhangxueming on 15/5/25.
+//  Copyright (c) 2015年 zhangxueming. All rights reserved.
+//
+
+#include <stdio.h>
+//同一个工程里面main函数只有一个
+
+//注释代码
+
+//1. //
+//2. /* .... */
+
+//快捷键
+//command + / :注释(取消注释)代码
+//command + z :撤销
+//command + F : 查找
+//commang + Space: 切换输入法
+
+//control + N : 切换光标到下一行
+//control + P : 切换到上一行
+//control + E : 切换到行尾
+//control + A : 切换到行首
+//control + F : 向前移动一个字节
+//control + B : 向后移动一个字节
+
+
+//C99标准
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    /*printf("welcome\n");*/
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    return 0;
+}
+
+//c89标准
+
+//int main()
+//{
+//    printf("hello world\n");
+//    return 0;
+//}
+
+
+
+
